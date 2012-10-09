bb614b382e25680bb01d78dc59f2ce631f5389af
Adam Brewer <adamcbrewer@gmail.com>
2012-10-09 07:11:36 +0100

    Move namespacing to script.js and complete overhaul on index.php

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,8 +1,40 @@
-/* Author:
+/*
+ * Author:
+ * Your name here
+ */
 
-*/
-$(function () {
+(function(){
 
-	// here be codez
+	// Add some default values
+	window.S = {
+		basePath: document.body.getAttribute('data-base-url'),
+		userAgent: navigator.userAgent,
+		platform: navigator.platform
+	};
+
+
+	// Set up your functions here
+
+
+
+
+
+	// On page-ready
+	$(document).ready(function(){
+
+		// Add a data-attribute of the user-agent to <html> - very useful and worthwhile
+		// Target with: html[data-useragent*="Chrome/13.0"][data-platform="Win32"] ...
+		var b = document.documentElement;
+			b.setAttribute("data-useragent", Site.userAgent);
+			b.setAttribute("data-platform", Site.platform);
+
+
+		// Page is ready, start you stuff!
+
+
+
+
+
+	});
 
 }());
--- web/index.php
+++ web/index.php
@@ -12,7 +12,7 @@
 
 	<title></title>
 
-	<!-- META -->
+	<? // META ?>
 	<meta name="robots" content="all">
 
 	<link type="text/plain" rel="author" href="humans.txt" />
@@ -20,90 +20,44 @@
 	<meta name="description" content="">
 	<meta name="keywords" content="">
 
-	<!-- FACEBOOK OPEN GRAPH -->
-	<meta property="og:title" content=""/> <!-- subject title -->
-	<meta property="og:type" content=""/> <!-- More information: http://developers.facebook.com/docs/opengraph/ -->
-	<meta property="og:image" content=""/> <!-- image must be at least 50px by 50px with a maximum ratio of 3:1 -->
-	<meta property="og:url" content=""/> <!-- URL of the page being liked -->
-	<meta property="og:site_name" content=""/> <!-- A human-readable name for your site -->
-	<meta property="og:description" content=""/> <!-- basic site description -->
-	<!-- A comma-separated list of Facebook user IDs or application IDs -->
-	<meta property="fb:admins" content=""/> <!-- admin ID -->
-	<meta property="fb:app_id" content=""/> <!-- app ID -->
+	<? // FACEBOOK OPEN GRAPH ?>
+	<meta property="og:title" content=""/> <? // subject title ?>
+	<meta property="og:type" content=""/> <? // More information: http://developers.facebook.com/docs/opengraph/ ?>
+	<meta property="og:image" content=""/> <? // image must be at least 50px by 50px with a maximum ratio of 3:1 ?>
+	<meta property="og:url" content=""/> <? // URL of the page being liked ?>
+	<meta property="og:site_name" content=""/> <? // A human-readable name for your site ?>
+	<meta property="og:description" content=""/> <? // basic site description ?>
 
-
-	<!-- Mobile viewport optimized: j.mp/bplateviewport -->
-	<!-- Remove 'initial-scale' to prevent mobile devices zooming-in -->
+	<? // Mobile viewport optimized: j.mp/bplateviewport ?>
+	<? // Remove 'initial-scale' to prevent mobile devices zooming-in ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link href="assets/css/reset.css" rel="stylesheet"> <!-- reset with clearfix -->
-	<link href="assets/css/core.css" rel="stylesheet"> <!-- core stylesheet -->
+	<link href="assets/css/reset.css" rel="stylesheet"> <? // reset with clearfix ?>
+	<link href="assets/css/core.css" rel="stylesheet"> <? // core stylesheet ?>
 
-	<script src="assets/js/libs/modernizr-2.6.1.min.js"></script> <!-- Always check for latest version -->
+	<script src="assets/js/libs/modernizr-2.6.1.min.js"></script> <? // Always check for latest version ?>
 
-</head>
-<body>
-
-	<div id="wrapper">
-
-		<header id="header">
-
-			<nav id="navigation">
-
-			</nav> <!-- #navigation -->
-
-		</header> <!-- #header -->
-
-		<section role="main" id="content">
-
-			<article>
-
-			</article>
-
-		</section> <!-- #content -->
-
-		<footer id="footer">
-
-		</footer> <!-- #footer -->
-
-	</div> <!-- #wrapper -->
-
-	<script type="text/javascript" charset="utf-8">
-		/*
-		 * Init global object (for creating base_url methods, etc.)
-		 */
-		var Site = {
-			basePath: "<?php echo $_SERVER['HTTP_HOST']; ?>"
-		};
-
-		/*
-		 * Add a data-attribute of the user-agent to <html>
-		 *
-		 * Target with: html[data-useragent*="Chrome/13.0"][data-platform="Win32"] ...
-		 */
-		var b = document.documentElement;
-		b.setAttribute("data-useragent",  navigator.userAgent);
-		b.setAttribute("data-platform", navigator.platform );
-	</script>
-
-	<script src="assets/js/libs/jquery-1.7.2.min.js"></script>
-	<script src="assets/js/script.js"></script>
-
+	<? // Async Google Analytics ?>
 	<script type="text/javascript">
-
-		/*
 		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-XXXXXXXX-X']);
+		_gaq.push(['_setAccount', 'UA-XXXXXXXX-X']); <? // Add correct analytics ID ?>
 		_gaq.push(['_trackPageview']);
-
 		(function() {
 			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 		})();
-		*/
-
 	</script>
+
+</head>
+<body data-base-url="<?= $_SERVER['HTTP_HOST'] ?>">
+
+	<div id="wrapper">
+
+	</div> <!-- /wrapper -->
+
+	<script src="assets/js/libs/jquery-1.8.0.min.js"></script>
+	<script src="assets/js/script.js"></script>
 
 </body>
 </html>
