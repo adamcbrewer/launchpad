180648d247ce2cabd5fb0a978ff6ba9b8aae9be4
Adam Brewer <adamcbrewer@gmail.com>
2013-03-30 12:29:50 +0000

    Updated GA to new analytics.js snippet - https://developers.google.com/analytics/devguides/collection/analyticsjs/

--- web/index.php
+++ web/index.php
@@ -34,16 +34,18 @@
 
 	<script src="assets/js/libs/modernizr-2.6.2.custom.js"></script> <? // Always check for latest version ?>
 
-	<? // Async Google Analytics ?>
+	<?
+		// Async Google Analytics
+		// https://developers.google.com/analytics/devguides/collection/analyticsjs/
+	?>
 	<script type="text/javascript">
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-XXXXXXXX-X']); <? // Add correct analytics ID ?>
-		_gaq.push(['_trackPageview']);
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-XXXX-Y'); <? // correct account number ?>
+		ga('send', 'pageview');
 	</script>
 
 </head>
